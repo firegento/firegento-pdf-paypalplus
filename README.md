7fe7d78991fa9ef979962afc85ec8dea1a4bfe2f
Simon Sprankel <simonsprankel@gmail.com>
2016-03-05 14:19:10 +0100

    first release

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# firegento-pdf-paypalplus
+FireGento PDF PayPalPlus
+========================
+FireGento Pdf PayPalPlus adds the detailed PayPalPlus payment information on the FireGento PDF invoices.
+
+Facts
+-----
+- version: 1.0.0
+- extension key: FireGento_PdfPayPalPlus
+- [extension on GitHub](https://github.com/firegento/firegento-pdf-paypalplus)
+- [direct download link](https://github.com/firegento/firegento-pdf-paypalplus/archive/master.zip)
+
+Description
+-----------
+FireGento Pdf PayPalPlus adds the detailed PayPalPlus payment information on the FireGento PDF invoices.
+
+Requirements
+------------
+- PHP >= 5.4.0
+- FireGento_Pdf
+- Iways_PayPalPlus
+- Mage_Core
+- Mage_Payment
+- Mage_Sales
+
+Compatibility
+-------------
+- Magento >= 1.6
+
+Installation Instructions
+-------------------------
+1. Copy all the src files into your document root.
+2. Clear the cache.
+
+Uninstallation
+--------------
+1. Remove all extension files from your Magento installation.
+
+Support
+-------
+If you have any issues with this extension, open an issue on [GitHub](https://github.com/firegento/firegento-pdf-paypalplus/issues).
+
+Contribution
+------------
+Any contribution is highly appreciated. The best way to contribute code is to open a [pull request on GitHub](https://help.github.com/articles/using-pull-requests). In order to contribute to the latest code, please checkout the `development` branch after cloning your fork.
+
+Developer
+---------
+FireGento team and all other [contributors](https://github.com/firegento/firegento-pdf-paypalplus/contributors)
+
+License
+-------
+[GNU General Public License, version 3 (GPLv3)](http://opensource.org/licenses/gpl-3.0)
+
+Copyright
+---------
+(c) 2016 FireGento Team
---
+++ src/app/code/community/FireGento/PdfPayPalPlus/Model/Observer.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This file is part of a FireGento e.V. module.
+ *
+ * This FireGento e.V. module is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This script is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * PHP version 5
+ *
+ * @category  FireGento
+ * @package   FireGento_PdfPayPalPlus
+ * @author    FireGento Team <team@firegento.com>
+ * @copyright 2016 FireGento Team (http://www.firegento.com)
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ */
+
+/**
+ * Adds the detailed PayPalPlus payment information in the comments section of the FireGento PDF invoices.
+ *
+ * @category  FireGento
+ * @package   FireGento_PdfPayPalPlus
+ * @author    FireGento Team <team@firegento.com>
+ */
+class FireGento_PdfPayPalPlus_Model_Observer
+{
+
+    /**
+     * Adds the detailed PayPalPlus payment information in the comments section of the FireGento PDF invoices.
+     *
+     * @param Varien_Event_Observer $observer
+     *
+     * @return $this
+     */
+    public function firegentoPdfInvoiceInsertNote(Varien_Event_Observer $observer)
+    {
+        /** @var Mage_Sales_Model_Order $order */
+        $order = $observer->getOrder();
+
+        $paymentMethod = $order->getPayment()->getMethod();
+        if ($paymentMethod->getCode != Iways_PayPalPlus_Model_Payment::METHOD_CODE) {
+            return $this;
+        }
+
+        $result = $observer->getResult();
+        $notes  = $result->getNotes();
+
+        /** @var Iways_PayPalPlus_Block_Payment_Info $paymentBlock */
+        $paymentBlock = Mage::helper('payment')->getInfoBlock($order->getPayment());
+        $paymentText  = $paymentBlock->toPdf();
+        $paymentText  = str_replace('{{pdf_row_separator}}', '', $paymentText);
+        $notes[]      = $paymentText;
+
+        $result->setNotes($notes);
+
+        return $this;
+    }
+
+}
